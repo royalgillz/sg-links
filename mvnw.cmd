10c90128e4149966828462544f40f8b325600008
Sehaj Gill <92250502+royalgillz@users.noreply.github.com>
2026-02-24 02:58:39 +0000

    fix: remove non-existent flyway-database-postgresql dep (Flyway 9 only needs flyway-core)

--- mvnw.cmd
+++ mvnw.cmd
@@ -1,54 +1,35 @@
-@REM ----------------------------------------------------------------------------
-@REM Licensed to the Apache Software Foundation (ASF) under one
-@REM or more contributor license agreements.  See the NOTICE file
-@REM distributed with this work for additional information
-@REM regarding copyright ownership.  The ASF licenses this file
-@REM to you under the Apache License, Version 2.0 (the
-@REM "License"); you may not use this file except in compliance
-@REM with the License.  You may obtain a copy of the License at
-@REM
-@REM    https://www.apache.org/licenses/LICENSE-2.0
-@REM
-@REM Unless required by applicable law or agreed to in writing,
-@REM software distributed under the License is distributed on an
-@REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-@REM KIND, either express or implied.  See the License for the
-@REM specific language governing permissions and limitations
-@REM under the License.
-@REM ----------------------------------------------------------------------------
-
-@REM ----------------------------------------------------------------------------
-@REM Apache Maven Wrapper startup batch script, version 3.2.0
-@REM ----------------------------------------------------------------------------
+@REM Maven Wrapper startup batch script
+@SETLOCAL EnableDelayedExpansion
 
 @IF "%__MVNW_ARG0_NAME__%"=="" (SET "BASE_DIR=%~dp0") ELSE (SET "BASE_DIR=%__MVNW_ARG0_NAME__%")
 
 @SET MAVEN_PROJECTBASEDIR=%BASE_DIR%
-@IF NOT "%MAVEN_BASEDIR%"=="" SET MAVEN_PROJECTBASEDIR=%MAVEN_BASEDIR%
+@IF NOT "%MAVEN_BASEDIR%"=="" SET "MAVEN_PROJECTBASEDIR=%MAVEN_BASEDIR%"
 
-@SET WRAPPER_JAR=%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.jar
-@SET WRAPPER_PROPERTIES=%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.properties
+@REM Strip trailing backslash
+@IF "!MAVEN_PROJECTBASEDIR:~-1!"=="\" SET "MAVEN_PROJECTBASEDIR=!MAVEN_PROJECTBASEDIR:~0,-1!"
 
-@IF NOT EXIST "%WRAPPER_JAR%" (
-    FOR /F "usebackq tokens=1,2 delims==" %%A IN ("%WRAPPER_PROPERTIES%") DO (
-        IF "%%A"=="wrapperUrl" SET WRAPPER_URL=%%B
+@SET "WRAPPER_JAR=!MAVEN_PROJECTBASEDIR!\.mvn\wrapper\maven-wrapper.jar"
+@SET "WRAPPER_PROPERTIES=!MAVEN_PROJECTBASEDIR!\.mvn\wrapper\maven-wrapper.properties"
+
+@IF NOT EXIST "!WRAPPER_JAR!" (
+    FOR /F "usebackq tokens=1,2 delims==" %%A IN ("!WRAPPER_PROPERTIES!") DO (
+        IF "%%A"=="wrapperUrl" SET "WRAPPER_URL=%%B"
     )
-    IF NOT "%WRAPPER_URL%"=="" (
-        IF EXIST "%JAVA_HOME%\bin\java.exe" (
-            "%JAVA_HOME%\bin\java.exe" -classpath "" org.apache.maven.wrapper.BootstrapMainStarter "%WRAPPER_URL%" "%WRAPPER_JAR%"
-        ) ELSE (
-            java -classpath "" org.apache.maven.wrapper.BootstrapMainStarter "%WRAPPER_URL%" "%WRAPPER_JAR%"
-        )
+    IF NOT "!WRAPPER_URL!"=="" (
+        powershell -Command "Invoke-WebRequest -Uri '!WRAPPER_URL!' -OutFile '!WRAPPER_JAR!'"
     )
 )
 
 @IF EXIST "%JAVA_HOME%\bin\java.exe" (
-    SET JAVA_CMD="%JAVA_HOME%\bin\java.exe"
+    SET "JAVA_CMD=%JAVA_HOME%\bin\java.exe"
 ) ELSE (
-    SET JAVA_CMD=java
+    SET "JAVA_CMD=java"
 )
 
-@%JAVA_CMD% %MAVEN_OPTS% %MAVEN_DEBUG_OPTS% ^
-  -classpath "%WRAPPER_JAR%" ^
-  "-Dmaven.multiModuleProjectDirectory=%MAVEN_PROJECTBASEDIR%" ^
+"!JAVA_CMD!" %MAVEN_OPTS% %MAVEN_DEBUG_OPTS% ^
+  -classpath "!WRAPPER_JAR!" ^
+  "-Dmaven.multiModuleProjectDirectory=!MAVEN_PROJECTBASEDIR!" ^
   org.apache.maven.wrapper.MavenWrapperMain %*
+
+@ENDLOCAL
